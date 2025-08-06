d9e0475893339d0a846ecb87e48d6efe1f24463c
ASISDEV <asissahoo120@gmail.com>
2025-08-06 18:35:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const display = document.querySelector("#display");
+
+let displayString = "";
+
+function appendToDisplay(input) {
+    displayString += input;
+
+    let formatedString = displayString
+    formatedString = formatedString.replaceAll("/100*", "%");
+    formatedString = formatedString.replaceAll("/", "÷");
+    formatedString = formatedString.replaceAll("*", "×");
+    display.value = formatedString;
+      display.scrollLeft = display.scrollWidth;
+}
+
+function calculate() {
+    if(!displayString){
+        return
+    }
+
+    try {
+        displayString = eval(displayString);
+        display.value = displayString;
+    } catch (error) {
+        display.value = "Error!";
+        displayString = ""
+    }
+
+}
+
+function clearDisplay() {
+    displayString = "";
+    display.value = displayString;
+}
+
+function cut() {
+    displayString = displayString.slice(0, -1);
+    display.value = display.value.slice(0, -1);
+}
